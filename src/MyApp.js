58fc8539d4e752cd0d00c87c25edb3841800f235
Ro-Bert-Web <robertbhs424@gmail.com>
2021-01-08 18:28:14 -0800

    Added Submit Form

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,23 +1,24 @@
-import React, {useState} from 'react';
-import Table from './Table'
+import React, {useState} from "react";
+import Form from "./Form";
+import Table from "./Table";
 
 function MyApp() {
 	const [characters, setCharacters] = useState([  
 		{
-			name: 'Charlie',
-			job: 'Janitor',
+			name: "Charlie",
+			job: "Janitor",
 		},
 		{
-			name: 'Mac',
-			job: 'Bouncer',
+			name: "Mac",
+			job: "Bouncer",
 		},
 		{
-			name: 'Dee',
-			job: 'Aspring actress',
+			name: "Dee",
+			job: "Aspring actress",
 		},
 		{
-			name: 'Dennis',
-			job: 'Bartender',
+			name: "Dennis",
+			job: "Bartender",
 		},
 	]);
 
@@ -28,9 +29,14 @@ function MyApp() {
 		setCharacters(update);
 	}
 
+	function updateList(person) {
+		setCharacters([...characters, person]);
+	}
+
 	return (
 		<div className = "container">
 			<Table characterData = {characters} removeCharacter = {removeOneCharacter} />
+			<Form handleSubmit = {updateList} />
 		</div>
 	);
 }
