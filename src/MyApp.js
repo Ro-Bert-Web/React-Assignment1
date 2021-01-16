c5fc5fca4d92e2948f969d2fbd95b602ad79a258
Ro-Bert-Web <robertbhs424@gmail.com>
2021-01-15 22:00:06 -0800

    Paired with Backend

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,6 +1,7 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Form from "./Form";
 import Table from "./Table";
+import axios from "axios";
 
 function MyApp() {
 	const [characters, setCharacters] = useState([  
@@ -22,15 +23,64 @@ function MyApp() {
 		},
 	]);
 
-	function removeOneCharacter (index) {
-		const update = characters.filter((character, i) => {
-			return i !== index;
+	async function fetchAll() {
+		try {
+			const response = await axios.get("http://localhost:5000/users");
+			return response.data.users_list;
+		}
+		catch (error) {
+			console.log("error");
+			return false;
+		}
+	}
+
+	useEffect(() => {
+		fetchAll().then(result => {
+			if (result) {
+				setCharacters(result);
+			}
 		});
-		setCharacters(update);
+	}, []);
+
+	async function makePostCall(person) {
+		try {
+			const response = await axios.post("http://localhost:5000/users", person);
+			return response;
+		}
+		catch (error) {
+			console.log(error);
+			return false;
+		}
+	}
+
+	async function makeDelCall(id) {
+		try {
+			const response = await axios.delete("http://localhost:5000/users/".concat(id));
+			return response;
+		}
+		catch(error) {
+			console.log(error);
+			return false;
+		}
+	}
+
+	function removeOneCharacter (index, id) {
+		console.log(index, id);
+		makeDelCall(id).then(result => {
+			if (result) {
+				const update = characters.filter((character, i) => {
+					return i !== index;
+				});
+			setCharacters(update);
+			}
+		});
 	}
 
 	function updateList(person) {
-		setCharacters([...characters, person]);
+		makePostCall(person).then(result => {
+			if (result)
+				setCharacters([...characters, result.data]);
+		});
 	}
 
 	return (
--- src/Table.js
+++ src/Table.js
@@ -18,8 +18,9 @@ function TableBody (props) {
 			<tr key={index}>
 				<td>{row.name}</td>
 				<td>{row.job}</td>
+				<td>{row.id}</td>
 				<td>
-					<button onClick = {() => props.removeCharacter(index)}>Delete</button>
+					<button onClick = {() => props.removeCharacter(index, row.id)}>Delete</button>
 				</td>
 			</tr>
 		);
